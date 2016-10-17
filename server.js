a3b2e09b0043a0454d8018fd86739f66639cc081
at-rooshee <ru_sha_nia@yahoo.ca>
2016-10-16 23:49:51 -0500

    Testing Event-driven asynchronous server-side JavaScript with callbacks in action

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
+function onRequest(request, response) {
+  console.log("Request received.");
+  response.writeHead(200, {"Cotnent-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log("Server has started.");
