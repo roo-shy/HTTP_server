6b0c2b094ef523b31966e194cf359dccce9b27a7
at-rooshee <ru_sha_nia@yahoo.ca>
2016-10-26 22:47:13 -0500

    Adding router.js and requestHandlers

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var server = require("./server");
-var router = require("/router");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
 
-server.start(router, route);
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+nandle["/upload"] = requestHandlers.upload;
+
+server.start(router.route);
